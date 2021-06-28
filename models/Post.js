b3dec4bfdcbaa7d7d4aca243dabc18ee26142f30
Ryan Lynn Rosas <rltrosas88@gmail.com>
2021-06-28 11:33:44 -0500

    3.4 create the post model

---
+++ models/Post.js
@@ -0,0 +1,57 @@
+//3.4 step ONE connection to MySQL stored in connection.js and the Mode and Datatypes used from the sequelize package
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+//3.4 step TWO define the post model
+// create our Post model
+class Post extends Model {}
+
+//3.4 step THREE define the columns in the Post, 
+    //configure the naming conventions, and
+    //pass the current connection instance to initialize the Post model.
+// create fields/columns for Post model
+Post.init(
+    {
+        //identified the id column as the primary key and set it to auto-increment
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        //defined the title column as a String value
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        //defined the post_url as a String and insured the url is a verified link by setting the isURL
+        post_url: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isURL: true
+            }
+        },
+        //determines who posted the news article by using 
+            //the references property we establish the relationship between this post and the user by creating 
+                //a reference to the User model, specifically to the id column that is defined by the key property
+            //the user_id is defined as the foreigm key and will be the matching link
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    //configure the meta data, including the naming conventions
+    {
+      sequelize,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'post'
+    }
+  );
+
+  //3.4 step FOUR make the Post model accessible to other parts of the applicaiont
+  module.exports = Post;
--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
 //1.5 Step Three
 //collect and export the User model data
 const User = require('./User');
+//3.4 step FIVE require the Post model
+const Post = require("./Post");
 
-module.exports = { User };
+module.exports = { User, Post };
