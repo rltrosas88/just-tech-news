dff8cbfe0888a5a4937896ed78b0a38845a02048
Ryan Lynn Rosas <rltrosas88@gmail.com>
2021-06-30 15:31:30 -0500

    5.4 create the API routes

---
+++ routes/api/comment-routes.js
@@ -0,0 +1,44 @@
+//5.4 step ONE define the comments own endpoints
+const router = require('express').Router();
+const { Comment } = require('../../models');
+
+router.get('/', (req, res) => {
+    //5.4 step FOUR
+    
+});
+
+router.post('/', (req, res) => {
+    //5.4 step THREE 
+    Comment.create({
+        comment_text: req.body.comment_text,
+        user_id: req.body.user_id,
+        post_id: req.body.post_id
+    })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+});
+
+router.delete('/:id', (req, res) => {
+    //5.4 step FIVE
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbCommentData => {
+            if (!dbCommentData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+            res.json(dbCommentData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
--- routes/api/index.js
+++ routes/api/index.js
@@ -3,7 +3,10 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes.js');
 //3.6 step SEVEN add the postRoutes
 const postRoutes = require('./post-routes');
+//5.4 step TWO import the comment-routes.js
+const commentRoutes = require('./comment-routes');
 
+router.use('/comments', commentRoutes);
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 
