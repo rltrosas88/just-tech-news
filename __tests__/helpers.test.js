8499db48d8a249f78cc1f3d4361cd9d441354f72
Ryan Lynn Rosas <rltrosas88@gmail.com>
2021-07-06 18:11:30 -0500

    14.4.6 write and fulfill test for plural words

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,5 +1,5 @@
 // 14.4.5 step THREE import the format_date() function
-const {format_date} = require('../utils/helpers');
+const {format_date, format_plural} = require('../utils/helpers');
 
 // 14.4.5 step ONE
     //install npm i jest -D
@@ -11,3 +11,12 @@ test('format_date() returns a date string', () => {
   
     expect(format_date(date)).toBe('3/20/2020');
   });
+
+// 14.4.6 step ONE write a test to ensure that format_plural() correctly pluralizes words
+test('format_plural() returns correct pluralized words', () => {
+    const word1 = format_plural('lion', 1);
+    const word2 = format_plural('tiger', 2);
+
+    expect(word1).toBe('lion');
+    expect(word2).toBe('tigers');
+});
--- utils/helpers.js
+++ utils/helpers.js
@@ -4,8 +4,16 @@
     //use the date object to avoid bringing in unnecessary dependencies
 module.exports = {
     format_date: date => {
-      return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
-        date
-      ).getFullYear()}`;
-    }
-  }
+        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
+            date
+        ).getFullYear()}`;
+    },
+    //14.4.6 step TWO add a format_plural() method
+    format_plural: (word, amount) => {
+        if (amount !== 1) {
+            return `${word}s`;
+        }
+    
+        return word;
+      }
+};
