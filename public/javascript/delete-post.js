7a3f657fe87a8f6f3f5c33709381ec1b0cd740e2
Ryan Lynn Rosas <rltrosas88@gmail.com>
2021-07-06 20:47:17 -0500

    14.5.6 create an edit post page

---
+++ public/javascript/delete-post.js
@@ -0,0 +1,23 @@
+// 14.5.6 step SIX set up the button-click handler
+async function deleteFormHandler(event) {
+    event.preventDefault();
+    // 14.5.6 step SEVEN 
+        //capture the id of the post 
+        //use fetch() to make a DELETE request to /api/posts/:id
+            //if the request is successful, redirect the user using document.location.replace('/dashboard/')
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'DELETE'
+    });
+            
+    if (response.ok) {
+        document.location.replace('/dashboard/');
+    } else {
+        alert(response.statusText);
+    }
+}
+  
+  document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
